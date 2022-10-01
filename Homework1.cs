fb171a0ef744b77acf8fb61f1775ba1b6731d139
evsDen <saratov642000@gmail.com>
2022-10-01 13:38:45 +0400

    Создание репозиторияя с Домашним заданием

---
+++ Homework1.cs
@@ -0,0 +1,43 @@
+// Задача №2
+//  Console.Write("Введите первое число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число:");
+// int b = Convert.ToInt32(Console.ReadLine());
+//     if (a == b)
+//         Console.WriteLine("Числа равны.");
+//     if (a > b)
+//         Console.WriteLine("Первое число является максимальным");
+//     if (a < b)
+//         Console.WriteLine("Второе число является максимальным");
+
+// Задача №4
+// Console.Write("Введите первое число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число:");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число:");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = 0;
+// max = a;
+// if (b > a)
+//     max = b;
+// if (c > max)
+//     max = c;
+// Console.WriteLine(max);
+
+// Задача №6
+// Console.Write("Введите число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 2==0)
+//      Console.WriteLine("Число четное.");
+// else
+//     Console.WriteLine("Число нечетное.");
+
+
+// Задача №8
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i <= n; i++)
+//    if (i % 2==0)
+//     Console.Write(i + " ");
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// квадрат числа
+// Console.Write("Введите первое число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число:");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (b == a*a)
+//      Console.WriteLine("Второе число является квадратом первого");
+// else 
+//     Console.WriteLine("Второе число не является квадратом первого");
+// 
+
+
+// Дни недели
+// Console.Write("Введите день недели: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// while (n <1 || n > 7)
+// {
+//        Console.Write("Вы ошиблись! Введите день недели:");
+//        n = Convert.ToInt32(Console.ReadLine());
+// }
+// if (n == 1)
+//    Console.WriteLine("Понедельник");
+// if (n == 2)
+//    Console.WriteLine("Вторник");
+// if (n == 3)
+//    Console.WriteLine("Среда");
+// if (n == 4)
+//    Console.WriteLine("Четверг");
+// if (n == 5)
+//    Console.WriteLine("Пятница");
+// if (n == 6)
+//    Console.WriteLine("Суббота");
+// if (n == 7)
+//    Console.WriteLine("Воскресенье");
+
+// Вывод всех чисел от -N до N
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = (-1) * n; i <= n; i++)
+//     Console.Write(i + " ");
+
+//  Вывод последнего числа
+//  Console.Write("Введите число: ");
+//  int n = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine(n % 10);
